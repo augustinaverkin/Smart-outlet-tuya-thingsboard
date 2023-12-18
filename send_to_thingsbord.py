a5678fa3ceae249012bf42fcf4c7d5fb9732f8fc
augustinaverkin <99637094+augustinaverkin@users.noreply.github.com>
2023-12-18 23:24:24 +0200

    Update send_to_thingsbord.py

--- send_to_thingsbord.py
+++ send_to_thingsbord.py
@@ -23,7 +23,7 @@ def send_Thingsboard_price(price,curr_power,id):
         # MQTT broker configuration
         broker_url = "thingsboard.cloud"
         broker_port = 1883  # Default MQTT port
-        #username = "2EO0t4sN6Dh7ksz6Iwz4"
+        
         username = "USERNAME"
         topic = "v1/devices/me/telemetry"
         #payload = '{"temperature"}100}'
